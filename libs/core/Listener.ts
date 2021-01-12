7ba5b9f899fc297a2a223a3f340031a6632361fd
偏差 <1075095689@qq.com>
2021-01-12 17:02:31 +0800

    Update Listener.ts

--- libs/core/Listener.ts
+++ libs/core/Listener.ts
@@ -6,26 +6,26 @@ import Exception from './Exception'
  * 实现事件监听  发布订阅模式
  */
 class Listener {
-	protected $eventMap: any = {}
-	protected $config: any = {}
+	protected _eventMap: any = {}
+	protected _config: any = {}
 	constructor(config: IDataBase) {
-		this.$config = config
+		this._config = config
 	}
 	public listen(name: string, fn: Function): Listener {
-		const keys: string[] = toKeys(this.$config)
+		const keys: string[] = toKeys(this._config)
 		if (!keys.length) {
 			throw new Exception('没有找到MySql配置')
 		}
-		if (this.$eventMap[name]) {
-			this.$eventMap[name].push(fn)
+		if (this._eventMap[name]) {
+			this._eventMap[name].push(fn)
 			return this
 		}
-		this.$eventMap[name] = [fn]
+		this._eventMap[name] = [fn]
 		return this
 	}
 
 	public emit(name: string, ...args): Listener {
-		const listen: any[] = this.$eventMap[name] || []
+		const listen: any[] = this._eventMap[name] || []
 		each(listen, (fn) => {
 			fn(...args)
 		})
